c6cf66a7c98b1ecf7534d421a15cf7c65af4d4ca
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2026-03-25 18:43:29 +0000

    basic features done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Player {
-    constructor(y, dead, money, jumpHeight, speed, name, level, xp, gravity){
+    constructor(x, y, dead, money, jumpHeight, speed, name, level, xp, width, height){
         this.y = y;
         this.dead = dead;
         this.money = money
@@ -9,8 +9,11 @@ class Player {
         this.level = level;
         this.xp = xp;
         this.yVel = 0;
+        this.x = x
         this.onGround = false;
         this.gravity = 0.5;
+        this.width = width;
+        this.height = height
     }
 
     move(){
@@ -19,13 +22,13 @@ class Player {
 
     jump(){
         if(this.onGround){
-            this.yVel-=15
+            this.yVel-=this.jumpHeight
         }
 
     }
 
     draw(img){
-        image(img, width/4, this.y, 100, 100)
+        image(img, this.x, this.y, this.width, this.height)
     }
 
     applyPhysics() {
@@ -34,18 +37,25 @@ class Player {
 
         // Ground collision (height - 150 accounts for sprite size)
         let groundY = height *5/6; 
-        if (this.y >= groundY- 80) {
-            this.y = groundY-80;
+        if (this.y >= groundY- this.height*4/5) {
+            this.y = groundY-this.height*4/5;
             this.yVel = 0;
             this.onGround = true;
+            this.gravity = 0.5
         }else{
             this.onGround = false;
         }
     }
 
     update(img){
+        if(!this.dead){
         this.draw(img);
         this.applyPhysics()
+        }
+    }
+
+    fastFall(){
+        this.gravity*=3
     }
 }
 
@@ -59,7 +69,6 @@ class Spike {
 
     move(){
         this.x-=screenSpeed
-        console.log(this.x)
     }
 
     draw(img){
@@ -127,17 +136,26 @@ function preload() {
     rabbit = loadImage('/rabbit.png')
 }
 
-function spawnSpikes(arr, random){
-    if(random < 0.05){
+function spawnSpikes(arr, random, timer){
+    timer++;
+    if(random === 0 && timer > 150){
         let spikeArrange = new spikeArrangement(4, [1, 1, 1, 1], width*1.4, height*5/6, width/30, height/15);
         spikeArrange.createArr()
         arr.push(spikeArrange)
+        timer = 0;
+    }else if(random === 1 && timer > 150){
+        let spikeArrange = new spikeArrangement(4, [0.7, 2, 2, 0.7], width*1.4, height*5/6, width/30, height/15);
+        spikeArrange.createArr()
+        arr.push(spikeArrange)
+        timer = 0;
     }
+    console.log(timer, random)
+    return timer;
 }
 
 function deleteSpikes(arr){
     for(let i = 0; i < arr.length; i++){
-        if(arr[i].arrOfSpikes[0].x < height*(-1/2)){
+        if(arr[i].arrOfSpikes[0].x < width*(-1/2)){
             arr.splice(i, 1)
         }
     }
@@ -152,50 +170,55 @@ function updateAllSpikes(arr, img){
 }
 
 function checkCollisions(arr, mainCharacter){
-    for(let i = 0; i < arr.lenth; i++){
+    for(let i = 0; i < arr.length; i++){
         let spikes = arr[i].arrOfSpikes;
-        for(let j = 0; j < spikes.length;i ++){
-            if(spikes[i].x < mainCharacter.x && mainCharacter.y < )
+        for(let j = 0; j < spikes.length;j++){
+            let spike = spikes[j];
+            if(mainCharacter.x < spike.x + spike.width && mainCharacter.x + mainCharacter.width*3/5 > spike.x && mainCharacter.y < spike.y + spike.height && mainCharacter.y + mainCharacter.height*3/5 > spike.y){
+                mainCharacter.dead = true;
+            }
         }
     }
 }
 
 function setup() {
-  let width = windowWidth;
-  let height = windowHeight;
-  createCanvas(width, height - 50);
-
+  createCanvas(windowWidth, windowHeight - 50);
+    mainCharacter = new Player(100, 100, false, 0, height/60, 0, "asdfasdf", 0, 0, width/10, height/10)
 }
-
     let screenSpeed = 10;
-    let mainCharacter = new Player(100, false, 0, 100, 100, "john", 0, 0);
     let x = 0;
     let spike = new Spike(1000, 100, 100, 100)
     let spikeArray = [];
     let score = 0;
-
+    let timer = 0;
 function draw() {
-    let rand = random(0, 10)
-    x+=screenSpeed ;
+    let rand = Math.floor(random(0, 100))
+    if(mainCharacter.dead){
+        screenSpeed = 0;
+    }else{
+        x+=screenSpeed ;
+        screenSpeed+=0.01;
+    }
     let newX = x % width
     clear();
-    console.log(spikeArray)
     image(background, -newX, 0, width, height);
     image(background, width-newX, 0, width, height);
     mainCharacter.update(rabbit)
-    spawnSpikes(spikeArray, rand);
+    mainCharacter.x = width/4;
+    timer = spawnSpikes(spikeArray, rand, timer);
     updateAllSpikes(spikeArray, spikeImage);
+    checkCollisions(spikeArray, mainCharacter)
     deleteSpikes(spikeArray)
-    spike.y = height*3/4 
-    spike.update(img)
     score++; 
     text('SCORE: ' + score, 50, 50);
-
 }
 
 function keyPressed() {
-  if (key === ' ') {
+  if (key === 'w') {
     mainCharacter.jump();
+  }
+  if(key === 's'){
+    mainCharacter.fastFall();
   }
 
 }
